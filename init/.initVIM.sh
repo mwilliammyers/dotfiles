8c5be5196534164a3d81e42102bbc6163a0b85b8
William Myers <mkwmms@icloud.com>
2015-03-13 16:06:58 -0600

    corrected custom vim files directory to use ${HOME}

--- init/.initVIM.sh
+++ init/.initVIM.sh
@@ -9,11 +9,11 @@ NC='\033[0m' # No Color
 
 #TODO update this when this gets merged into master also make it multi-line...
 echo -e "${color}Installing spf-vim framework...${NC}"
-REPO_BRANCH="cleanup-bootstrap" REPO_URI='https://github.com/jrobeson/spf13-vim.git' APP_PATH=${vim_spf13} sh <(curl https://raw.githubusercontent.com/jrobeson/spf13-vim/cleanup-bootstrap/bootstrap.sh -L)
+#REPO_BRANCH="cleanup-bootstrap" REPO_URI='https://github.com/jrobeson/spf13-vim.git' APP_PATH=${vim_spf13} sh <(curl https://raw.githubusercontent.com/jrobeson/spf13-vim/cleanup-bootstrap/bootstrap.sh -L)
 
-#TODO:setup custom consolidated VIM dir see .vimrc.before line 59ish
 echo -e "${color}Linking vim dotfiles from ${vim_src} to ${vim_dst}...${NC}"
 ln -sfv 2> /dev/null "${vim_src}"/.* ~
+echo let g:spf13_consolidated_directory = \'${HOME}/.vim\' >> ~/.vimrc.before
 
 #install other handy VIM plugins
 brew install ctags
@@ -28,7 +28,7 @@ function installYCM() {
 function installEclim() {
     cd "${init}"
     #TODO: handle if eclim is already installed...
-    
+    # TODO: set $ECLIPSE_HOME here? 
     echo -e "${color}Downloading latest version of Eclim...${NC}"
     curl \
         --location --get --progress-bar \
@@ -47,13 +47,15 @@ function installEclim() {
 # TODO: option to install powerline fonts...
 
 # TODO: add -q option to silence this and NOT install by default...
-read -t 20 -p "Install eclim? (y/n) " -n 1
+read -r -t 20 -p "Install eclim? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     isntallEclim;
 fi
 
+echo -e ""
+
 # TODO: add -q option to silence this and NOT install by default...
-read -t 20 -p "Install the YouCompleteMe completion engine binary? (y/n) " -n 1
+read -r -t 20 -p "Install the YouCompleteMe completion engine binary? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     isntallYCM;
 fi
