d44c038353bad931b84959b3356b6aa8dfbfdcb3
william <mkwmms@users.noreply.github.com>
2015-06-25 13:21:14 -0600

    Update fpath.zsh

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -3,5 +3,4 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ( $ZSH/* ); do
     [ -d "$topic_folder" ] && fpath=($topic_folder $fpath)
-done 
-[ -d $ANTIBODY_HOME/jolux-antigen-hs-git ] && fpath=($ANTIBODY_HOME/jolux-antigen-hs-git $fpath)
+done
