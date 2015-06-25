540373c6c16404e9faebf9674bec8b4be1dcf9cd
william <mkwmms@users.noreply.github.com>
2015-06-25 14:08:57 -0600

    Update config.zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -55,6 +55,7 @@ if [[ ! -z "$+terminfo[smkx]" ]] && [[ ! -z "$+terminfo[rmkx]" ]]; then
   zle -N zle-line-finish
 fi
 
+# Key bindings ################################
 # Use emacs key bindings
 bindkey -e
 
@@ -109,4 +110,33 @@ else
   bindkey "\e[3~" delete-char
 fi
 
+
+# Plugin settings ################################
+# autosuggestions settings ##########
+export AUTOSUGGESTION_HIGHLIGHT_CURSOR=1
+export AUTOSUGGESTION_HIGHLIGHT_COLOR='fg=240'
+export AUTOSUGGESTION_ACCEPT_RIGHT_ARROW=1
+
+# zsh-history-substring-search ######
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "[kcuu1]" history-substring-search-up
+bindkey "[kcud1]" history-substring-search-down
+
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
+export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias tip: "
+
+export GENCOMPL_FPATH=${XDG_DATA_HOME:-${HOME}}/.completions
 
