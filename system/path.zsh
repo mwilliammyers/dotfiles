3186c6dc1410cb8227a3f50d27297c3d04cf1e38
William Myers <wmyers7@m7int01.fsl.byu.edu>
2015-08-26 14:29:00 -0600

    added R_LIBS to linux path

--- system/path.zsh
+++ system/path.zsh
@@ -11,7 +11,6 @@ export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc.py"
 export CURL_HOME="${XDG_CONFIG_HOME}/curl"
 # export ATOM_HOME="${XDG_CONFIG_HOME}/atom"
 
-# TODO: find away around needing this check...
 if [[ "$OSTYPE" =~ ^(linux)+ ]]; then
   default_path=/usr/local/cuda/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/opt/ganglia/bin:/opt/ganglia/sbin:/opt/ibutils/bin:/usr/java/latest/bin:/opt/rocks/bin:/opt/rocks/sbin:/opt/dell/srvadmin/bin
   export PATH="/bluehome3/wmyers7/.fslbuild/bin:/bluehome3/wmyers7/.fslbuild/sbin:/fslhome/wmyers7/.linuxbrew/opt/go/libexec/bin:/fslhome/wmyers7/.local/bin:${default_path}"
@@ -39,23 +38,13 @@ if [[ "$OSTYPE" =~ ^(linux)+ ]]; then
     unset HOMEBREW_CACHE HOMEBREW_BREW_FILE 
   }
 
-  export GOPATH=${XDG_BIN_HOME}
-  export PYENV_ROOT=/bluehome3/wmyers7/.linuxbrew/var/pyenv
+  export GOPATH="${XDG_BIN_HOME}"
+  export PYENV_ROOT="${HOME}/.linuxbrew/var/pyenv"
+  export R_LIBS="${XDG_DATA_HOME}/lib/r"
+  export PERL_CPANM_HOME="/apps/perlmodules/hcpanm"
 
-  # Perl setup
-  perl_module_root="/apps/perlmodules"
-  #export PERLBREW_ROOT=${perl_module_root}/perl5
-  ##source ${PERLBREW_ROOT}/etc/bashrc
-  export PERL_CPANM_HOME=${perl_module_root}/cpanm
-  #export PERL5LIB="${PERLBREW_ROOT}/lib/perl5:${PERLBREW_ROOT}/lib/perl5/x86_64-linux-thread-multi/Linux"
-  #export PERL_MB_OPT="--install_base = ${PERLBREW_ROOT}"
-  #export PERL_MM_OPT="INSTALL_BASE=${PERLBREW_ROOT}"
-  #export PERLBREW_HOME=${PERLBREW_ROOT}/.perlbrew
-  #export PERL_LOCAL_LIB_ROOT=${PERLBREW_ROOT}
-
-  #source ~/.linuxbrew/etc/grc.bashrc #FIXME: this is hardcoded
   source /usr/share/Modules/init/zsh
-  source ~/.modules
+  # source ~/.modules
 elif [[ "$OSTYPE" =~ ^(darwin)+ ]]; then
   export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/Users/wm/bin:/usr/local/libexec/gnubin:/opt/local/bin:/opt/local/sbin:/usr/local/opt/go/libexec/bin"
   export MANPATH="/usr/local/share/man:/usr/share/man"
