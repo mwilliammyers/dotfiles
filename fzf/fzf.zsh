4de999a2ee26c6abc9d7e3b6a21e7156fdddc021
William Myers <wmyers7@m7int02.fsl.byu.edu>
2015-08-26 14:43:34 -0600

    fixed fzf paths

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,20 +1,20 @@
-zf_home="$XDG_DATA_HOME/vim/plugged/fzf"
+fzf_home="${XDG_DATA_HOME}/vim/plugged/fzf"
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "$fzf_home/bin" ]]; then
-  export PATH="$PATH:$fzf_home/bin"
+if [[ ! "$PATH" =~ "${fzf_home}/bin" ]]; then
+  export PATH="$PATH:${fzf_home}/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "$fzf_home/man" && -d "$fzf_home/man" ]]; then
-  export MANPATH="$MANPATH:$fzf_home/man"
+if [[ ! "$MANPATH" =~ "${fzf_home}/man" && -d "$fzf_home/man" ]]; then
+  export MANPATH="$MANPATH:${fzf_home}/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "$fzf_home/shell/completion.zsh" 2> /dev/null
+[[ $- =~ i ]] && source "${fzf_home}/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$fzf_home/shell/key-bindings.zsh"
+source "${fzf_home}/shell/key-bindings.zsh"
