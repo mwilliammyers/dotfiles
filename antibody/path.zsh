66d2c1d38ef42aa12979098008fcfddf2f714db1
william <mkwmms@users.noreply.github.com>
2015-09-06 14:47:15 -0600

    Moving to home-brew tap based antibody install

--- antibody/path.zsh
+++ antibody/path.zsh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
 
-#[[ "$OSTYPE" =~ ^(darwin)+ ]] && gopath="/usr/local" || gopath="${HOME}/.local"
-#source "${gopath}/src/github.com/mkwmms/antibody.zsh"
-
 export ANTIBODY_HOME="${XDG_CONFIG_HOME}/antibody"
 export ANTIBODY_BUNDLE_FILE=${ZSH}/antibody/bundles
-source "${ZSH}/antibody/antibody/antibody.zsh"
+
+# TODO: do not hardcode these:
+if [[ "$OSTYPE" =~ ^(linux)+ ]]; then
+  source "$HOME/.linuxbrew/share/antibody.zsh"
+elif [[ "$OSTYPE" =~ ^(darwin)+ ]]; then
+  source "/usr/local/share/antibody.zsh"
+fi
+  
