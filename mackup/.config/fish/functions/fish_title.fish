3271b3a4cbac438bc0c1c38a2ed96d09d11ac5f9
mkwmms <mkwmms@users.noreply.github.com>
2015-09-17 22:45:18 -0600

    Add files

--- mackup/.config/fish/functions/fish_title.fish
+++ mackup/.config/fish/functions/fish_title.fish
@@ -1,8 +1,1 @@
-function fish_title
-    set pure_pwd (pwd | command sed "s:^$HOME:~:") 
-    if test -n "$argv"
-	  echo -s (basename $pure_pwd) " : " $argv[1]
-    else
-      echo $pure_pwd
-    end
-end
+/Users/wm/.config/reel/mkwmms-pure/functions/fish_title.fish
