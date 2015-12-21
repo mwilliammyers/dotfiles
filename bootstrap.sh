a78651827c80bfb6c062143dba28d8a85553dd4e
mkwmms <mkwmms@users.noreply.github.com>
2015-12-21 00:36:30 -0800

    Update bootstrap script to use ansible playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,14 @@
 
 set -E
 
-ANSIBLE_CONFIG_DIR="$HOME/.config/ansible"
-ANSIBLE_CONFIG="$ANSIBLE_CONFIG_DIR/ansible.cfg"
-
-if [[ -d "$ANSIBLE_CONFIG_DIR/.git" ]]; then
-  pushd >/dev/null 2>&1 "$ANSIBLE_CONFIG_DIR" && git pull && popd >/dev/null 2>&1 
-else 
-  # TODO: separate Ansible bootstrapping (apart from the install itself) to own playbook
-  git clone https://gist.github.com/bcae313de14ab9a3ac66.git "$ANSIBLE_CONFIG_DIR"
-fi
-
 # Install dependencies if necessary
 hash ansible >/dev/null 2>&1 || pip install ansible --upgrade
 # pip install redis --upgrade -q
 
-export ANSIBLE_CONFIG="$ANSIBLE_CONFIG"
-
 echo "NOTE: If you intend to use fish as your default shell, remember to do: " \
   "set -U fish_user_paths (brew --prefix coreutils)/libexec/gnubin /usr/local/bin \$HOME/.local/bin \$fish_user_paths or something similar"
+
+ansible-playbook ansible.yml --ask-become-pass
 
 ansible-galaxy install -r requirements.yml
 
