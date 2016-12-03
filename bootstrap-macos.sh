a05c23086b82ccfb55d8930dc8faca5588640a19
mwilliammyers <mwilliammyers@users.noreply.github.com>
2016-12-03 14:54:24 -0700

    Update bootstrap-macos.sh
    - Create is_installed function
    - Simplify git clone

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -2,14 +2,16 @@
 
 dotfiles_dir="$HOME/Documents/developer/dotfiles"
 
-# install dependencies
-brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-ansible || brew install ansible
-git || brew install git
+is_installed() {
+  command -v "$1" >/dev/null 2>&1
+}
 
-mkdir -p "${dotfiles_dir}" \
-  && cd $(dirname "${dotfiles_dir}") \
-  && git clone git@github.com:mwilliammyers/dotfiles.git \
+# install dependencies
+is_installed brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+is_installed ansible || brew install ansible
+is_installed git || brew install git
+
+mkdir git clone git@github.com:mwilliammyers/dotfiles.git "${dotfiles_dir}" \
   && cd "${dotfiles_dir}"
 
 # bootstrap!
