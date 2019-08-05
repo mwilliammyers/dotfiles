5a91fdc2d68d64448ca9ab5d71120353816af7e4
William Myers <mwilliammyers@gmail.com>
2019-08-04 20:03:16 -0600

    Fix rust.sh

--- rust.sh
+++ rust.sh
@@ -2,14 +2,14 @@
 
 DOTFILES_BOOTSTRAP=false source ./bootstrap.sh
 
-cargo_path="$HOME/.cargo/bin/cargo"
+cargo_bin_path="$HOME/.cargo/bin"
 
-if ! [ -x  "$cargo_path" ]; then
+if ! [ -x  "$cargo_bin_path/cargo" ]; then
 	info "Installing rust..."
 	curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 fi
 
-command_is_executable fish >> /dev/null && fish -c "set -Ux fish_user_paths $cargo_path"
+command_is_executable fish >> /dev/null && fish -c "set -Ux fish_user_paths $cargo_bin_path"
 
 if [ "$(uname -s)" == "Darwin" ]; then
 	command_is_executable "clang" || xcode-select --install 2> /dev/null
@@ -22,7 +22,7 @@ fi
 # 	install_packages "openssl"
 # fi
 
-command "$cargo_path" install --force \
+command "$cargo_bin_path/cargo" install --force \
 	cargo-watch \
 	cargo-edit \
 	cargo-bloat \
