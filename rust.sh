b23735c9026131452197a66e0eb49a322dc16b0b
William Myers <mwilliammyers@gmail.com>
2019-08-04 18:53:46 -0600

    Update rust.sh

--- rust.sh
+++ rust.sh
@@ -2,17 +2,21 @@
 
 DOTFILES_BOOTSTRAP=false source ./bootstrap.sh
 
-if ! command_is_executable cargo; then
+cargo_path="$HOME/.cargo/bin/cargo"
+
+if ! [ -x  "$cargo_path" ]; then
 	info "Installing rust..."
 	curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 fi
 
-command_is_executable fish && fish -c "set -Ux fish_user_paths $HOME/.cargo/bin/cargo"
+command_is_executable fish >> /dev/null && fish -c "set -Ux fish_user_paths $cargo_path"
 
-$HOME/.cargo/bin/cargo install \
+command "$cargo_path" install --force \
 	cargo-watch \
 	cargo-edit \
 	cargo-bloat \
+	cargo-tree \
+	cargo-update \
 	ripgrep \
 	exa \
 	fd-find
