75fac68558e462e44c3a53acc6cd056152f27ff7
William Myers <mwilliammyers@gmail.com>
2019-08-05 18:47:14 -0600

    Update/fix iterm2 config

--- iterm2.sh
+++ iterm2.sh
@@ -9,8 +9,3 @@ command_is_executable brew >> /dev/null \
 
 DOTFILES_HOMEBREW_CASK=true ./install.sh $package
 
-# TODO: rely on $HOME being set?
-source_config_dir="$DOTFILES_DIR/config/$package"
-dest_config_dir=$HOME/Library/Application\ Support/$package
-
-configure_single_package "$source_config_dir" "$dest_config_dir"
