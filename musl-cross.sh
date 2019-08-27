7d3351be6c8dfff522c2f5ede1e5425290c08b34
William Myers <mwilliammyers@gmail.com>
2019-08-27 14:41:28 -0600

    Add cargo settings to musl-cross.sh

--- musl-cross.sh
+++ musl-cross.sh
@@ -9,7 +9,16 @@ command_is_executable brew \
 
 # TODO: check all executables
 if ! command_is_executable x86_64-linux-musl-gcc; then
+    # FIXME: DOTFILES_HOMEBREW_OPTS
     DOTFILES_HOMEBREW_OPTS="--with-arm-hf --with-aarch64 --with-arm"
 
     install_packages "${package}" || die "Installing ${package} failed"
+fi
+
+if command_is_executable cargo >/dev/null 2>&1; then
+    cat >>~/.cargo/config <<EOF
+
+[target.x86_64-unknown-linux-musl]
+linker = "x86_64-linux-musl-gcc"
+EOF
 fi
