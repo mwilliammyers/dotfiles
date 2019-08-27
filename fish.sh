5dc93837d8e0e86d60301c219a985140830dfb57
William Myers <mwilliammyers@gmail.com>
2019-08-26 23:05:02 -0600

    Update fish.sh

--- fish.sh
+++ fish.sh
@@ -12,6 +12,12 @@ if ! [ "$SHELL" = "$fish" ]; then
 	sudo chsh -s "${fish_path}"  $USER
 fi
 
+# TODO: handle Linux mountpoints
+if [ "$(uname -s)" == "Darwin" ]; then
+	# TODO: check if file exists first?
+	cp -r /Volumes/config/$USER/.local/share/fish ~/.local/share/
+fi
+
 info "Installing fisher..."
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish \
 	|| die "Installing fisher failed"
