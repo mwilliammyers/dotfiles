46590f5b3ab133d6d13ebd107dfa3fb9d71a5d0c
William Myers <mwilliammyers@gmail.com>
2019-08-27 13:01:30 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -194,8 +194,8 @@ if is_truthy "${DOTFILES_BOOTSTRAP:-1}"; then
     ./fish.sh
     ./git.sh
     ./install.sh python3 fzf
-    # ./nodejs.sh
-    # ./neovim.sh
+    ./nodejs.sh
+    ./neovim.sh
     # ./sublime-text.sh
 
     if [ "$(uname -s)" == "Darwin" ]; then
