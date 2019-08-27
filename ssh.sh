8c2125257c796707f10cfe2eb2300825ae4ba5c6
William Myers <mwilliammyers@gmail.com>
2019-08-26 23:08:07 -0600

    Create temporary ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DOTFILES_BOOTSTRAP=false source ./bootstrap.sh
+
+# TODO: we really should be generating this instead...
+# TODO: handle Linux mountpoints
+if [ "$(uname -s)" == "Darwin" ]; then
+    # TODO: check if file exists first?
+    cp -r /Volumes/config/$USER/.ssh ~/
+fi
+
