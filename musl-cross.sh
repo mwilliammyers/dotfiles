042723b3764bce2cda8b2786640b058b2f3c933d
William Myers <mwilliammyers@gmail.com>
2019-08-27 14:44:16 -0600

    Add check before updating cargo config

--- musl-cross.sh
+++ musl-cross.sh
@@ -16,9 +16,11 @@ if ! command_is_executable x86_64-linux-musl-gcc; then
 fi
 
 if command_is_executable cargo >/dev/null 2>&1; then
+    if ! grep -q "x86_64-linux-musl-gcc" ~/.cargo/config
     cat >>~/.cargo/config <<EOF
 
 [target.x86_64-unknown-linux-musl]
 linker = "x86_64-linux-musl-gcc"
 EOF
+    fi
 fi
