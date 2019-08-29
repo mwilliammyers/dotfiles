177ebabd8f4183c13194f9008f3bf50d0033b487
William Myers <mwilliammyers@gmail.com>
2019-08-29 11:07:03 -0600

    Fix check for determining login shell

--- fish.sh
+++ fish.sh
@@ -8,14 +8,24 @@ install_packages_if_necessary "${package}" || die "Installing ${package} failed"
 
 fish_path=$(which fish)
 
-if ! [ "$SHELL" = "$fish" ]; then
+login_shell="unknown"
+if [ "$(uname -s)" == "Darwin" ]; then
+    login_shell="$(finger $USER | perl -n -e'/.*Shell\:\s+(.*)/ && print $1')"
+elif [ "$(uname -s)" == "Darwin" ]; then
+    login_shell="$(getent passwd $LOGNAME | cut -d: -f7)"
+fi
+
+if ! [ "$login_shell" = "$fish_path" ]; then
+    info "Changing the login shell to fish..."
     sudo chsh -s "${fish_path}"  $USER
+else
+    info "The login shell is already fish"
 fi
 
 # TODO: handle Linux mountpoints
 if [ "$(uname -s)" == "Darwin" ]; then
     # TODO: check if file exists first?
-    cp -fr /Volumes/config/$USER/.local/share/fish ~/.local/share/
+    cp -fr "/Volumes/config/$USER/.local/share/fish" ~/.local/share/ 2>/dev/null
 fi
 
 info "Installing fisher..."
