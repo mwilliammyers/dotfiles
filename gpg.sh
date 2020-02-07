aeb78836b1ee72abcb2a94f3440f46b9ee86d686
William Myers <mwilliammyers@gmail.com>
2020-02-07 15:35:10 -0700

    Improve gpg.sh
    - Add GPG_USER env var check
    - Add xdg-open support
    - Always modify git config
    - Add user.signingkey to git config

--- gpg.sh
+++ gpg.sh
@@ -25,22 +25,28 @@ fi
 # https://github.com/NicoHood/gpgit#12-key-generation
 # https://lists.gnupg.org/pipermail/gnupg-users/2017-December/059622.html
 
-# `--quick-generate-key` will exit 2 if user_id already has a key
-if gpg --quick-generate-key "William Myers <mwilliammyers@gmail.com>" future-default default 0; then
-    key_id="$(gpg --list-secret-keys --keyid-format LONG --with-colons | awk -F: '/^sec:/ { print $5 }')"
+user="${GPG_USER:-William Myers <mwilliammyers@gmail.com>}"
 
+# `--quick-generate-key` will exit 2 if user_id already has a key
+if gpg --quick-generate-key "$user" future-default default 0; then
     if [ -x "$(command -v pbcopy)" ]; then
-        gpg --armor --export "$key_id" | pbcopy
+        gpg --armor --export "$user" | pbcopy
     elif [ -x "$(command -v xsel)" ]; then
-        gpg --armor --export "$key_id" | xsel -ib
+        gpg --armor --export "$user" | xsel -ib
     fi
 
     info "Attempting to open browser; check clipboard for GPG public key..."
-
-    open "https://github.com/settings/gpg/new"
-
-    if command_is_executable git; then
-        git config --global commit.gpgsign true
-        # git config --global user.signingkey "$key_id"
+    if [ -x "$(command -v open)" ]; then
+        open "https://github.com/settings/gpg/new"
+    elif [ -x "$(command -v xdg-open)" ]; then
+        xdg-open "https://github.com/settings/gpg/new"
     fi
 fi
+
+if command_is_executable git; then
+    key_id=$(gpg --list-secret-keys --keyid-format LONG --with-colons "$user" | awk -F: '/^sec:/ { print $5 }')
+    
+    git config --global commit.gpgsign true
+    git config --global user.signingkey "$key_id"
+fi
+
