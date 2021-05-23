0eb85682fc0f44898dc3c5a32029f099a8f26276
William Myers <mwilliammyers@gmail.com>
2021-05-23 15:04:54 -0700

    Update fd.sh

--- bootstrap/fd.sh
+++ bootstrap/fd.sh
@@ -11,10 +11,10 @@ if ! command_is_executable fd; then
     install_packages $package || die "Installing $package failed"
 fi
 
-if [ -x "$(command -v fdfind)" ] && [ ! -x "$(command -v fd)" ]; then
-    if [ -x "$(command -v apt-get)" ]; then
-        sudo update-alternatives --install /usr/bin/fd fd "$(command -v fdfind)" 1
-    elif [ -x "$(command -v dnf)" ]; then
-        sudo alternatives --install /usr/bin/fd fd "$(command -v fdfind)" 1
-    fi
-fi
+# if [ -x "$(command -v fdfind)" ] && [ ! -x "$(command -v fd)" ]; then
+#     if [ -x "$(command -v apt-get)" ]; then
+#         sudo update-alternatives --install /usr/bin/fd fd "$(command -v fdfind)" 1
+#     elif [ -x "$(command -v dnf)" ]; then
+#         sudo alternatives --install /usr/bin/fd fd "$(command -v fdfind)" 1
+#     fi
+# fi
